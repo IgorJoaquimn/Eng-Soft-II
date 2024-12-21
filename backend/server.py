ff4266de0a7f8e9407a950e581f41fa08b3022ba
IgorJoaquimn <igor-joaquim@ufmg.br>
2024-12-21 18:25:06 -0300

    fix: backend refactor

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from extractInfo import getInfosFromText
-from utils import process_file, validate_file_input, UPLOAD_FOLDER
+from utils import process_request, UPLOAD_FOLDER
 
 app = Flask(__name__)
 CORS(app)
@@ -12,26 +11,8 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 @app.route('/api/submit', methods=['POST'])
 def submit_data():
     try:
-        text = ""
-        if request.is_json:
-            data = request.get_json()
-            text = data.get('text', "")
-        
-        # Check if it's a multipart form data request (file upload)
-        elif request.files:
+        return process_request(request) 
 
-            error_message = validate_file_input(request)
-            if error_message != "":
-                return jsonify({"error": error_message}), 400
-            
-            text = process_file(request)
-        
-        # If neither JSON nor file
-        else:
-            return jsonify({"error": "Invalid request format"}), 400
-        
-        return jsonify(getInfosFromText(text))
-    
     except Exception as e:
         print(f"Error processing request: {e}")
         return jsonify({"error": str(e)}), 500
--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,25 @@
 import os
 from flask import jsonify
+from extractInfo import getInfosFromText
 
 UPLOAD_FOLDER = 'uploads'
+
+def process_request(request):
+    if request.is_json:
+        data = request.get_json()
+        text = data.get('text', "")
+        return jsonify(getInfosFromText(text))
+
+    # Check if it's a multipart form data request (file upload)
+    if request.files:
+        error_message = validate_file_input(request)
+        if error_message != "":
+            return jsonify({"error": error_message}), 400
+
+        text = process_file(request)
+        return jsonify(getInfosFromText(text))
+
+    return jsonify({"error": "Invalid request format"}), 400
 
 def process_file(request):
 
@@ -14,18 +32,18 @@ def process_file(request):
         content = f.read(1024 * 1024)
 
         return content
-    
+
 def validate_file_input(request):
 
     file = request.files.get('file')
     if not file:
         return "No file received"
-    
+
     if not file.filename:
         return "Empty filename"
 
     allowed_extensions = {'txt', 'pdf', 'docx', 'doc'}
     if not ('.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in allowed_extensions):
         return "Unsupported file type"
-    
+
     return ""
